d05514cb70294ca480511cc50123a851da752dc5
scottastanley <ScottAStanley@yahoo.com>
2023-07-15 21:08:30 -0700

    Cleaning up imports

--- src/main/java/com/bb/neo4j_login_module/Neo4jUser.java
+++ src/main/java/com/bb/neo4j_login_module/Neo4jUser.java
@@ -1,9 +1,7 @@
 package com.bb.neo4j_login_module;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.jetty.jaas.JAASRole;
 import org.eclipse.jetty.security.UserPrincipal;
